deb75085cbe84747451d18198bbe812285bb9820
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-18 22:21:22 +0900

    🐣 create firebase.js

---
+++ src/service/firebase.js
@@ -0,0 +1,5 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACR_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACR_APP_FIREBASE_PROJECT_ID,
+};
