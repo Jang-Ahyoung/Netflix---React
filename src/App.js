f627b456deb55cc3c6ac61bef7411a792f2dc198
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-19 21:37:39 +0900

    🛠 init setting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
