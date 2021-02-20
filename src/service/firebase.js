514f9aa4e6d72e1bfdd5e1e9c3eaec1712dbe7e8
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-20 18:47:33 +0900

    📜 fetch data from TMDB

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACR_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACR_APP_FIREBASE_PROJECT_ID,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
