2108644969879f58bd1639155e90d3624a1caf27
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-02-21 12:53:29 +0900

    🎡 HomeScreen Design

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import requests from '../../service/request';
 import Banner from '../Banner/Banner';
 import Navbar from '../Navbar/Navbar';
+import Row from '../Row/Row';
 import styles from './HomeScreen.module.css';
 
 function HomeScreen() {
@@ -8,6 +10,16 @@ function HomeScreen() {
         <div className={styles.homeScreen}>
             <Navbar />
             <Banner />
+            <Row title = 'NETFLIX ORIGINALS'
+                 fetchUrl = {requests.fetchNetflixOriginals} isLargeRow/>
+            <Row title = 'TOP Rated' fetchUrl = {requests.fetchTopRated}/>
+            <Row title = 'Action Movies' fetchUrl = {requests.fetchActionMovies}/>
+            <Row title = 'Comedy Movies' fetchUrl = {requests.fetchComedyMovies}/>
+            <Row title = 'Horror Movies' fetchUrl = {requests.fetchHorrorMovies}/>
+            <Row title = 'Ramance Movies' fetchUrl = {requests.fetchRomanceMovies}/>
+            <Row title = 'Document Movies' fetchUrl = {requests.fetchDocumentaries}/>
+            
+
 
         </div>
     )
